a3199f39398ae7bd2ded0346ccb964f8cc97073b
rdyoung <rdyoung@iastate.edu>
2014-04-04 19:52:16 -0500

    Added VS solution
    Contains the project VS solution

---
+++ turretController/turretController/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuildDefender;
+
+namespace turretController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MissileLauncher myLauncher = new MissileLauncher();
+            myLauncher.command_Down(10);
+        }
+    }
+}
